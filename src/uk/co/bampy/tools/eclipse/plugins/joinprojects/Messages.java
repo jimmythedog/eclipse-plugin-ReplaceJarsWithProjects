a90ba875e2faca8aafd7ac80f939eeede7ddfc2e
jimmythedog <github@kgibbs.co.uk>
2012-12-20 16:20:30 +0000

    Imported project from local svn server

---
+++ src/uk/co/bampy/tools/eclipse/plugins/joinprojects/Messages.java
@@ -0,0 +1,24 @@
+package uk.co.bampy.tools.eclipse.plugins.joinprojects;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS
+{
+    private static final String BUNDLE_NAME = "uk.co.bampy.tools.eclipse.plugins.joinprojects.messages"; //$NON-NLS-1$
+    public static String ReplaceJarsWithProjectsHandler_0;
+    public static String ReplaceJarsWithProjectsHandler_1;
+    public static String ReplaceJarsWithProjectsHandler_2;
+    public static String ReplaceJarsWithProjectsHandler_3;
+    public static String ReplaceJarsWithProjectsHandler_4;
+    public static String ReplaceJarsWithProjectsHandler_5;
+    public static String ReplaceJarsWithProjectsHandler_6;
+    static
+    {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages()
+    {
+    }
+}
